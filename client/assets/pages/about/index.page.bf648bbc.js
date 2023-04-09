c90e7c720be1e4f78b3a6cf1bb6d476465a0a98f
judixel <judixel2015@ya.ru>
2023-04-09 22:54:00 +0400

    Adding dist

---
+++ client/assets/pages/about/index.page.bf648bbc.js
@@ -0,0 +1,1 @@
+import{_ as n}from"../../chunks/c27b6911.js";import{q as o,p as s,F as _,t as e,x as t}from"../../chunks/e91d47fc.js";const a={},c=e("h1",null,"About",-1),r=e("p",null,[t("Example of app using "),e("code",null,"vite-plugin-ssr"),t(".")],-1);function l(p,i){return s(),o(_,null,[c,r],64)}const x=n(a,[["render",l]]);export{x as default};
